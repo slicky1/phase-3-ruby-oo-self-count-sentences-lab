a0e77cf28379bb9f5fdeaf5bc7f8723e3f97de4c
slicky1 <okunonke@gmail.com>
2021-07-11 22:32:02 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
+  end
 
+  def count_sentences_0
+    raw = self.split(/\. |\! |\? /)
+    return 0 if raw == []
+    returns = raw.size
+    returns -= 1 if raw.last[-1].match(/\.|\!|\?/) == nil #!= "." && "?" && "!"
+    returns
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
